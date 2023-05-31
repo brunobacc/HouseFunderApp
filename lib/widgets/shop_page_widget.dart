fe2f036eaad58e7867be339d6bbab4ddb99ec324
Manuel Ferreira <93943279+manuca17@users.noreply.github.com>
2023-05-31 10:22:10 +0100

    mudança shop widget

--- lib/widgets/shop_page_widget.dart
+++ lib/widgets/shop_page_widget.dart
@@ -35,26 +35,59 @@ class _ShopPageWidgetState extends State<ShopPageWidget> {
                     ),
                     itemBuilder: (BuildContext context, int index) {
                       Product product = products.list[index];
-                      return Stack(
-                        children: [
-                          Card(
-                            margin: EdgeInsets.fromLTRB(10, 10, 10, 20),
-                            child: Image.asset(product.image),
-                          ),
-                          Align(
-                            alignment: Alignment.bottomLeft,
-                            child: Padding(
-                              padding: EdgeInsets.only(left: 10),
-                              child: Text(product.title),
+                      return Card(
+                        elevation: 4,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Stack(
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: Image.asset(
+                                product.image,
+                                fit: BoxFit.cover,
+                              ),
                             ),
-                          ),
-                          Positioned(
-                            bottom: 25,
-                            right: 25,
-                            child: Text('${product.price}Points',
-                                style: TextStyle(color: Colors.white)),
-                          ),
-                        ],
+                            Positioned(
+                              bottom: 0,
+                              left: 0,
+                              right: 0,
+                              child: Container(
+                                padding: const EdgeInsets.symmetric(
+                                    vertical: 8, horizontal: 12),
+                                decoration: BoxDecoration(
+                                  color: Colors.black54,
+                                  borderRadius: BorderRadius.only(
+                                    bottomLeft: Radius.circular(10),
+                                    bottomRight: Radius.circular(10),
+                                  ),
+                                ),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      product.title,
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    SizedBox(height: 4),
+                                    Text(
+                                      '${product.price} Points',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 14,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
                       );
                     },
                   )
