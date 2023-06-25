4ff21affa273168a658ad8f978a1f760b518ec15
manuca17 <gokiferreira2@gmail.com>
2023-06-25 16:13:04 +0100

    aa

---
+++ lib/widgets/admin/admin_validate_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AdminValidatePage extends StatefulWidget {
+  const AdminValidatePage({super.key});
+
+  @override
+  State<AdminValidatePage> createState() => _AdminValidatePageState();
+}
+
+class _AdminValidatePageState extends State<AdminValidatePage> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+
+    );
+  }
+}
